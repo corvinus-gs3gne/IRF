93672cf46a265c5bad0e91358f913703d8da2b19
corvinus-gs3gne <fanni.buki@stud.uni-corvinus.hu>
2021-10-05 20:43:40 +0200

    2-6. csatlakozás, orm, adatok betöltése, excel eszközök behivatkozása, excel mf létrehozása

--- negyedikhet_gs3gne/negyedikhet_gs3gne/Form1.cs
+++ negyedikhet_gs3gne/negyedikhet_gs3gne/Form1.cs
@@ -7,14 +7,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace negyedikhet_gs3gne
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+        Excel.Application xlApp;
+        Excel.Workbook xlWBook;
+        Excel.Worksheet xlWSheet;
+        
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+            CreateExcel();
         }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
+
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                //Excel indítása, objektum betöltés
+
+                xlApp = new Excel.Application();
+
+                //új munkafüzet létrehoz
+
+                xlWBook = xlApp.Workbooks.Add(Missing.Value);
+
+                //új munkalap
+                xlWSheet = xlWBook.ActiveSheet;
+
+                //tábla létrehoz
+                CreateTable();
+
+                //control átadása felhasználónak
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+
+            }
+            catch (Exception ex) //hibakezelés beépített hibaüzivel
+            {
+
+                string errorMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errorMsg, "Error");
+
+                //Excel app automat bezárása hiba esetén
+
+                xlWBook.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWBook = null;
+                xlApp = null;
+            }
+        }
+
+        
     }
 }
