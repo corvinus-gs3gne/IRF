3912f8c420ea5f6a5ffa39699b55e4bb6fdc600d
corvinus-gs3gne <fanni.buki@stud.uni-corvinus.hu>
2021-10-08 11:49:13 +0200

    CreateTable function

--- negyedikhet_gs3gne/negyedikhet_gs3gne/Form1.cs
+++ negyedikhet_gs3gne/negyedikhet_gs3gne/Form1.cs
@@ -14,11 +14,15 @@ namespace negyedikhet_gs3gne
 {
     public partial class Form1 : Form
     {
+
+        private int _million = (int)Math.Pow(10, 6);
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
         Excel.Application xlApp;
         Excel.Workbook xlWBook;
         Excel.Worksheet xlWSheet;
+
+        
         
 
         public Form1()
@@ -51,6 +55,7 @@ namespace negyedikhet_gs3gne
 
                 //tábla létrehoz
                 CreateTable();
+                
 
                 //control átadása felhasználónak
 
@@ -72,6 +77,86 @@ namespace negyedikhet_gs3gne
                 xlApp = null;
             }
         }
+
+        
+        private void CreateTable()
+        {
+            string[] headers = new string[]
+            {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"
+                    
+
+
+            };
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlWSheet.Cells[1, i+1] = headers[i];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            int counter = 0;
+            int floorColumn = 6;
+
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                if (f.Elevator)
+                {
+                    values[counter, 4] = "Van";
+                }
+                else
+                {
+                    values[counter, 4] = "Nincs";
+                }                
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, floorColumn] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = String.Format("={0}/{1}*{2}",
+                    "H" + counter + 2.ToString(),
+                    GetCell(counter + 2, floorColumn+1) ,
+                    _million.ToString()); //"H2/G2*1000000"
+
+                counter++;
+            }
+
+            var range= xlWSheet.get_Range(
+                GetCell(2, 1),
+                GetCell(1+ values.GetLength(0), values.GetLength(1)));
+
+            range.Value2= values;
+        }
+
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
 
         
     }
