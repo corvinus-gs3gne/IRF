ad9bf76155fd0b2ffa93cf6d3da15e1f92878212
corvinus-gs3gne <fanni.buki@stud.uni-corvinus.hu>
2021-10-22 09:40:19 +0200

    2-5

--- week6_gs3gne/week6_gs3gne/Form1.cs
+++ week6_gs3gne/week6_gs3gne/Form1.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week6_gs3gne.Entities;
 
 namespace week6_gs3gne
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+            
         }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            //PortfolioItem p = new PortfolioItem(); bonyolultabb változat
+            //p.Index = "OTP";
+            //p.Volume = 10;
+            //Portfolio.Add(p);
+
+            dataGridView2.DataSource = Portfolio;
+        }
+
+        
     }
 }
