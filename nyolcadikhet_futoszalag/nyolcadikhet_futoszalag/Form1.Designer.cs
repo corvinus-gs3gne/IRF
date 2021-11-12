60ffb834a85afbc5102560c392c7c5acc4076d61
corvinus-gs3gne <fanni.buki@stud.uni-corvinus.hu>
2021-11-12 21:21:31 +0100

    Termékválasztó gombok

--- nyolcadikhet_futoszalag/nyolcadikhet_futoszalag/Form1.Designer.cs
+++ nyolcadikhet_futoszalag/nyolcadikhet_futoszalag/Form1.Designer.cs
@@ -33,6 +33,10 @@ namespace nyolcadikhet_futoszalag
             this.MainPanel = new System.Windows.Forms.Panel();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.carButton = new System.Windows.Forms.Button();
+            this.ballButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // MainPanel
@@ -40,9 +44,12 @@ namespace nyolcadikhet_futoszalag
             this.MainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.MainPanel.Controls.Add(this.label1);
+            this.MainPanel.Controls.Add(this.ballButton);
+            this.MainPanel.Controls.Add(this.carButton);
             this.MainPanel.Location = new System.Drawing.Point(1, 1);
             this.MainPanel.Name = "MainPanel";
-            this.MainPanel.Size = new System.Drawing.Size(787, 437);
+            this.MainPanel.Size = new System.Drawing.Size(887, 439);
             this.MainPanel.TabIndex = 0;
             // 
             // createTimer
@@ -57,14 +64,45 @@ namespace nyolcadikhet_futoszalag
             this.conveyorTimer.Interval = 10;
             this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick);
             // 
+            // carButton
+            // 
+            this.carButton.Location = new System.Drawing.Point(709, 31);
+            this.carButton.Name = "carButton";
+            this.carButton.Size = new System.Drawing.Size(75, 23);
+            this.carButton.TabIndex = 0;
+            this.carButton.Text = "CAR";
+            this.carButton.UseVisualStyleBackColor = true;
+            this.carButton.Click += new System.EventHandler(this.carButton_Click);
+            // 
+            // ballButton
+            // 
+            this.ballButton.Location = new System.Drawing.Point(709, 85);
+            this.ballButton.Name = "ballButton";
+            this.ballButton.Size = new System.Drawing.Size(75, 23);
+            this.ballButton.TabIndex = 1;
+            this.ballButton.Text = "Ball";
+            this.ballButton.UseVisualStyleBackColor = true;
+            this.ballButton.Click += new System.EventHandler(this.ballButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(709, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Coming next:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 452);
             this.Controls.Add(this.MainPanel);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -74,6 +112,9 @@ namespace nyolcadikhet_futoszalag
         private System.Windows.Forms.Panel MainPanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ballButton;
+        private System.Windows.Forms.Button carButton;
     }
 }
 
--- nyolcadikhet_futoszalag/nyolcadikhet_futoszalag/Form1.cs
+++ nyolcadikhet_futoszalag/nyolcadikhet_futoszalag/Form1.cs
@@ -16,11 +16,16 @@ namespace nyolcadikhet_futoszalag
     {
         private List<Toy> _toys = new List<Toy>();
         private IToyFactory _factory;
+        private Toy _nextToy;
 
         public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set
+            {
+                _factory = value;
+                DisplayNext();
+            }
         }
 
         public Form1()
@@ -32,7 +37,7 @@ namespace nyolcadikhet_futoszalag
         private void createTimer_Tick(object sender, EventArgs e)
         {
             var toy = Factory.CreateNew();
-            _toys.Add(toy);            
+            _toys.Add(toy);
             toy.Left = -toy.Width;
             MainPanel.Controls.Add(toy);
         }
@@ -50,7 +55,7 @@ namespace nyolcadikhet_futoszalag
                 }
             }
 
-            if (MaxPosition > 1000 )
+            if (MaxPosition > 1000)
             {
                 var oldestToy = _toys[0];
                 MainPanel.Controls.Remove(oldestToy);
@@ -58,6 +63,27 @@ namespace nyolcadikhet_futoszalag
             }
         }
 
-        
+        private void carButton_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void ballButton_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+            {
+                Controls.Remove(_nextToy);
+                _nextToy = Factory.CreateNew();
+                _nextToy.Top = label1.Top + label1.Height + 20;
+                _nextToy.Left = label1.Left;
+                Controls.Add(_nextToy);
+            }
+
+        }
     }
 }
