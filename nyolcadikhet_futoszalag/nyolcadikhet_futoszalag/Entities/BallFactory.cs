120df9b1a34cfbe07d3e468d0e3d1451e2d91d74
corvinus-gs3gne <fanni.buki@stud.uni-corvinus.hu>
2021-11-12 18:45:13 +0100

    Create BallFactory

---
+++ nyolcadikhet_futoszalag/nyolcadikhet_futoszalag/Entities/BallFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nyolcadikhet_futoszalag.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+
+
+}
